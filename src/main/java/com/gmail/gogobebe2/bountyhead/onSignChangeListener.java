ec3c43d6f7db7c42c3b8f746b35bf3b836fb25cd
gogobebe2 <gogobebe2@gmail.com>
2015-05-04 21:53:46 +1200

    1.0.8-DEVELOPMENT-SNAPSHOT

--- src/main/java/com/gmail/gogobebe2/bountyhead/onSignChangeListener.java
+++ src/main/java/com/gmail/gogobebe2/bountyhead/onSignChangeListener.java
@@ -1,7 +1,6 @@
 package com.gmail.gogobebe2.bountyhead;
 
 import org.bukkit.ChatColor;
-import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -13,22 +12,13 @@ public class onSignChangeListener implements Listener {
     @EventHandler(priority = EventPriority.NORMAL)
     public void onSignChange(SignChangeEvent event) {
         if (event.isCancelled()) return;
-        Sign sign = (Sign) event.getBlock().getState();
+        String[] lines = event.getLines();
         // TODO: check for permission.
-        if (createHeadSign(sign)) {
+        if (lines[0].equalsIgnoreCase("[sell]") && lines[1].equalsIgnoreCase("head")) {
+            event.setLine(0, ChatColor.DARK_BLUE + " [Sell] ");
+            event.setLine(1, " Head ");
             Player player = event.getPlayer();
             player.sendMessage(ChatColor.GREEN + "Head selling sign created!");
         }
-        event.getPlayer().sendMessage(sign.getLine(0) + " and " + sign.getLine(1));
-    }
-
-    private static boolean createHeadSign(Sign sign) {
-        if (sign.getLine(0).equalsIgnoreCase("[sell]") && sign.getLine(1).equalsIgnoreCase("head")) {
-            sign.setLine(0, ChatColor.DARK_BLUE + " [Sell] ");
-            sign.setLine(1, " Head ");
-            sign.update();
-            return true;
-        }
-        return false;
     }
 }
